bfa6334b4bf4d97b5b4479c052f6fd4195040445
DA\Daniil <Ridham@mail.ru>
2016-08-19 18:16:23 +0300

    COMMIT

---
+++ CalculateArea/CalculateArea.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateAreaFigures
+{
+    public class CalculateArea
+    {
+        public static double Triangle(int sideLength, int height)
+        {
+            return 0.5f * sideLength * height;
+        }
+
+        public static double Triangle(int sideLengthA, int sideLengthB, double angle)
+        {
+            angle = (Math.PI * angle) / 180; // перевод из градусов в радианы
+            return 0.5f * sideLengthA * sideLengthB * Math.Sin(angle);
+        }
+
+        public static double Quadrangle(int diagonalOne, int diagonaTwo, double angle)
+        {
+            angle = (Math.PI * angle) / 180; // перевод из градусов в радианы
+            return (diagonalOne * diagonaTwo * Math.Sin(angle)) / 2;
+        }
+        public static double Quadrangle(int sideLengthA, int sideLengthB, int sideLengthC, int sideLengthD)
+        {
+            float p = (sideLengthA + sideLengthB + sideLengthC + sideLengthD) / 2;
+            return Math.Sqrt((p - sideLengthA) * (p - sideLengthB) * (p - sideLengthC) * (p - sideLengthD));
+        }
+
+        public static int Square(int side)
+        {
+            return side * side;
+        }
+
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CalculateAreaFigures;
 
 namespace ConsoleApplication1
 {
@@ -10,37 +11,147 @@ namespace ConsoleApplication1
     {
         public static void Main(string[] args)
         {
-            int sideLength;            
-            do
-            {
-                Console.Write("Введите длину стороны треугольника: ");
-                if (int.TryParse(Console.ReadLine(), out sideLength))
-                    break;
-
-                Console.WriteLine("Вы ввели неверное значение! Введите пожалуйста число!");
-                
-            }
-            while (true);
-                        
-            int height;
-            do
-            {
-                Console.Write("Введите длину высоты проведенной к этой стороне: ");
-                if (int.TryParse(Console.ReadLine(), out height))
-                    break;
-
-                Console.WriteLine("Вы ввели неверное значение! Введите пожалуйста число!");
-            }
-            while (true);
-
-            float area = CalculateTriangleArea(sideLength, height);
-            Console.WriteLine("Площадь треугольника со стороной '{0}' и высотой '{1}', равняется '{2}'", sideLength, height, area);
+            Console.WriteLine("Выберите необходимую фигуру: \n 1 - Треугольник \n 2 - Четырехугольник \n 3 - Квадрат \n 4 - Выход ");
+            GeneralMenu(InputInt());
             Console.Read();
+
         }
 
-        private static float CalculateTriangleArea(int sideLength, int height)
-        {             
-             return 0.5f * sideLength * height;        
+        public static int InputInt()
+        {
+            int value;
+             do
+            {
+                if (int.TryParse(Console.ReadLine(), out value))
+                    break;
+
+                Console.WriteLine("Вы ввели неверное значение! Введите пожалуйста число!");
+            }
+            while (true);
+            return value;
         }
+        public static double InputDouble()
+        {
+            double value;
+            do
+            {
+                if (Double.TryParse(Console.ReadLine(), out value))
+                    break;
+
+                Console.WriteLine("Вы ввели неверное значение! Введите пожалуйста число!");
+            }
+            while (true);
+            return value;
+        }
+
+        public static void GeneralMenu(int caseSwitch)
+        {   
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Выберите формулу: \n 1 - Формула площади треугольника по стороне и высоте \n 2 - Формула площади треугольника по двум сторонам и углу между ними \n 3 - Назад");
+                    TriangleMenu(InputInt());
+                    break;
+                case 2:
+                    Console.WriteLine("Выберите формулу: \n 1 - Формула половины произведения диагоналей и синуса острого угла между ними \n 2 - Формула Брахмагупты \n 3 - Назад");
+                    QuadrangleMenu(InputInt());
+                    break;
+                case 3:
+                    Console.Write("Введите длину стороны квадрата: ");
+                    int side = InputInt();
+                    double area = CalculateArea.Square(side);
+                    Console.WriteLine("Площадь квадрата со стороной '{0}', равняется '{1}'", side,area);
+                    AnotherFigure();
+                    break;
+                case 4:
+                    break;
+                default:
+                    Console.WriteLine("Такого пункта меню нет, повторите ввод!");
+                    GeneralMenu(InputInt());
+                    break;
+            }
+        }
+
+        public static void TriangleMenu(int caseSwitch)
+        {           
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.Write("Введите длину стороны треугольника: ");
+                    int sideOne = InputInt();
+                    Console.Write("Введите длину высоты проведенной к этой стороне: ");
+                    int height = InputInt();
+                    double area = CalculateArea.Triangle(sideOne, height);
+                    Console.WriteLine("Площадь треугольника со стороной '{0}' и высотой '{1}', равняется '{2:0.00}'", sideOne, height, area);
+                    AnotherFigure();
+                    break;
+                case 2:
+                    Console.Write("Введите длину первой стороны треугольника: ");
+                    sideOne = InputInt();
+                    Console.Write("Введите длину второй стороны треугольника: ");
+                    int sideTwo = InputInt();
+                    Console.Write("Введите угол между сторонами: ");
+                    double angle = InputDouble();
+                    area = CalculateArea.Triangle(sideOne, sideTwo, angle);
+                    Console.WriteLine("Площадь треугольника с первой стороной '{0}', со второй стороной '{1}' и углом '{2}' равняется '{3:0.00}'", sideOne, sideTwo, angle, area);
+                    AnotherFigure();
+                    break;
+                case 3:
+                    Console.WriteLine("Выберите необходимую фигуру: \n 1 - Треугольник \n 2 - Четырехугольник \n 3 - Квадрат \n 4 - Круг");
+                    GeneralMenu(InputInt());
+                    break;
+                default:
+                    Console.WriteLine("Такого пункта меню нет, повторите ввод!");
+                    TriangleMenu(InputInt());
+                    break;
+            }
+        }
+        public static void QuadrangleMenu(int caseSwitch)
+        {
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.Write("Введите первую диагональ: ");
+                    int diagonalOne  = InputInt();
+                    Console.Write("Введите вторуб диагональ: ");
+                    int diagonalTwo = InputInt();
+                    Console.Write("Введите угол между диагоналями: ");
+                    double angle = InputDouble();
+                    double area = CalculateArea.Quadrangle(diagonalOne, diagonalTwo, angle);
+                    Console.WriteLine("Площадь четырехугольника с диагоналями '{0}' и '{1}', и углом '{2}' равняется '{3:0.00}'", diagonalOne, diagonalTwo, angle, area);
+                    AnotherFigure();
+                    break;
+                case 2:
+                    Console.WriteLine("Введите длину всех сторон(через enter):");
+                    int sideLengthA = InputInt();
+                    int sideLengthB = InputInt();
+                    int sideLengthC = InputInt();
+                    int sideLengthD = InputInt();
+                    area = CalculateArea.Quadrangle(sideLengthA, sideLengthB, sideLengthC, sideLengthD);
+                    Console.WriteLine("Площадь четырехугольника со сторонами '{0}','{1}', '{2}', '{3}', равняется '{4:0.00}'", sideLengthA, sideLengthB, sideLengthC, sideLengthD, area);
+                    AnotherFigure();
+                    break;
+                case 3:
+                    Console.WriteLine("Выберите необходимую фигуру: \n 1 - Треугольник \n 2 - Четырехугольник \n 3 - Квадрат \n 4 - Круг");
+                    GeneralMenu(InputInt());
+                    break;
+                default:
+                    Console.WriteLine("Такого пункта меню нет, повторите ввод!");
+                    QuadrangleMenu(InputInt());
+                    break;
+            }
+        }
+        public static void AnotherFigure()
+            {
+            Console.Write("Вы хотите найти площадь другой фигугры(да/нет)?");
+                    string answer = Console.ReadLine();
+                    if (answer.Contains("да"))
+                    {
+                        Console.WriteLine("Выберите необходимую фигуру: \n 1 - Треугольник \n 2 - Четырехугольник \n 3 - Квадрат \n 4 - Выход ");
+                        GeneralMenu(InputInt());
+                    }
+}
+
+
     }
 }
