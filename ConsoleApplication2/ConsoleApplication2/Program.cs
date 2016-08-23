a77db0000b3d01976254831bf9996fa39d2c8950
DA\Daniil <Ridham@mail.ru>
2016-08-23 20:19:04 +0300

    Ex2

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string binaryNumber = Convert.ToString(number, 2);
+            int counter = 0;
+            int result = 0;
+            for (int i = 0; i < binaryNumber.Length; i++)
+            {
+                if (binaryNumber[i] == '0')
+                {
+                    counter++;
+                    if(counter > result)
+                        result = counter;
+                }
+                else
+                    counter = 0;                 
+            }
+            Console.WriteLine(binaryNumber);
+            Console.WriteLine(result);
+            Console.Read();          
+        }
+    }
+}
